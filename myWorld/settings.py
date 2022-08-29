9796012e70f5794c4ef1693341b523aa7d26ba3f
Yonghoo Kim <dhdgn@naver.com>
2022-08-29 18:40:58 +0900

    changed: allow all host

--- myWorld/settings.py
+++ myWorld/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = config['SETTING']['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
